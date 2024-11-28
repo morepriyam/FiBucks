2d549d9859693f634e48bc47c27bd500336b2eee
priyam more <morepriyam99@gmail.com>
2024-11-28 17:31:29 -0500

    frontend categorised input

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,38 +1,38 @@
 import { DollarSign } from "lucide-react";
 
 export default function Navbar() {
-  return (
-    <header className="flex h-14 items-center px-4 lg:px-6">
-      <a className="flex items-center justify-center" href="/">
-        <DollarSign className="h-6 w-6" />
-        <span className="sr-only">FiBucks</span>
-      </a>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <a
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          Features
-        </a>
-        <a
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          Pricing
-        </a>
-        <a
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          About
-        </a>
-        <a
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          Contact
-        </a>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="flex h-14 items-center px-4 lg:px-6">
+			<a className="flex items-center justify-center" href="/">
+				<DollarSign className="h-6 w-6" />
+				<span className="sr-only">FiBucks</span>
+			</a>
+			<nav className="ml-auto flex gap-4 sm:gap-6">
+				<a
+					className="text-sm font-medium underline-offset-4 hover:underline"
+					href="#"
+				>
+					Features
+				</a>
+				<a
+					className="text-sm font-medium underline-offset-4 hover:underline"
+					href="#"
+				>
+					Pricing
+				</a>
+				<a
+					className="text-sm font-medium underline-offset-4 hover:underline"
+					href="#"
+				>
+					About
+				</a>
+				<a
+					className="text-sm font-medium underline-offset-4 hover:underline"
+					href="#"
+				>
+					Contact
+				</a>
+			</nav>
+		</header>
+	);
 }
