e91b45cfdc868b15b1f97ed6041af6c125a712b8
Parth Dalvi <parthdalvi3.1415@gmail.com>
2024-12-09 12:46:16 -0500

    signup verify tests

---
+++ test/verify.test.js
@@ -0,0 +1,34 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+
+chai.use(chaiHttp);
+const { request, expect } = chai;
+
+const baseUrl = "http://localhost:3000";
+
+describe("Simplified Email Verification API Test", () => {
+  it("should return 200 for successful email verification", (done) => {
+    const mockUserId = "anyUserId";
+    const mockToken = "anyToken";
+
+    request(baseUrl)
+      .get(`/api/users/verify?userId=${mockUserId}&token=${mockToken}`)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it("should return 400 for missing parameters", (done) => {
+    request(baseUrl)
+      .get("/api/users/verify")
+      .end((err, res) => {
+        if (err) return done(err);
+
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
